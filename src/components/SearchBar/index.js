b5a29a099e1076404f5f489e51c5ae532fcee8ea
fatihk29 <fatihkrc7@gmail.com>
2021-04-19 23:35:14 +0300

    color changed

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -11,7 +11,7 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: '#ECE',
+    backgroundColor: '#ECaE',
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15,
