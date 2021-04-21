1126a9580df658227f925f732b40b7a7a14f7404
fatihk29 <fatihkrc7@gmail.com>
2021-04-21 21:49:31 +0300

    vector icons

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const SearchBar = () => {
   return (
     <View style={styles.background}>
+      {/* <Icon size={24} color="black" name="search" /> */}
       <Text>Search Bar</Text>
     </View>
   );
@@ -11,7 +13,7 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: '#ECaE',
+    backgroundColor: '#ECAE',
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15,
