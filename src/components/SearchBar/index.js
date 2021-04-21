d1c1eb9a0b7132916151873e4c23bfeeef10fd5c
fatihk29 <fatihkrc7@gmail.com>
2021-04-21 22:06:43 +0300

    search icon added

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {View, TextInput, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      {/* <Icon size={24} color="black" name="search" /> */}
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Icon style={styles.iconStyle} color="black" name="search" />
+      <TextInput style={styles.inputStyle} placeholder="Search" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
+    marginTop: 8,
     backgroundColor: '#ECAE',
     height: 50,
     borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputStyle: {
+    fontSize: 14,
+    flex: 1,
+  },
+  iconStyle: {
+    fontSize: 30,
+    alignSelf: 'center',
     marginHorizontal: 15,
   },
 });
