ea940e408230058b19eba6ffeffdad744627f9ce
fatihk29 <fatihkrc7@gmail.com>
2021-04-22 23:04:09 +0300

    useEffect added

--- src/screens/SearchScreen/index.js
+++ src/screens/SearchScreen/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
 import SearchBar from '../../components/SearchBar';
 import yelp from '../../api/yelp';
@@ -6,22 +6,31 @@ import yelp from '../../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose',
-      },
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose',
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (error) {
+      setErrorMessage('something went wrong!');
+    }
   };
+
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
 
   return (
     <View>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
